61b24d9d47d2b1333ffe520af2068244ef050e16
Eline-DeV <157654635+Eline-DeV@users.noreply.github.com>
2024-01-28 17:47:50 +0100

    Update Make_Art_Function.R

--- Make_Art_Function.R
+++ Make_Art_Function.R
@@ -1,15 +1,18 @@
-devtools::install_github("djnavarro/flametree")
-library(flametree)
 
-#Adapted from flametree package. Adjustments allow for generation with seed input only
 
 make_art <- function(Seed){
+  library(circlize)
   set.seed(Seed)
-  art_colours <- sample(colors(distinct = FALSE), size = 3, replace = FALSE)
-  ran_time <- sample(c(1:10), size = 1)
-  ran_trees <- sample(c(1:25), size = 1)
-  data_structure <- flametree_grow(seed = Seed, time = ran_time, trees = ran_trees)
-  back_colours <- sample(colors(distinct = FALSE), size = 1)
-  plants <- sample(c("plain","voronoi", "wisp", "nativeflora", "minimal","themegray"), size = 1)
-  data_structure %>% flametree_plot(background = back_colours, palette = art_colours, style = plants)
+  art_colours <- sample(colors(distinct = FALSE), size = 8, replace = FALSE)
+  data_gen_nr <- sample(x = 1:100, size = 1, replace = FALSE)
+  vis_data <- matrix(data = rnorm(n = data_gen_nr), byrow = TRUE)
+  colors <- c(Row1 = art_colours[1], Col2 = art_colours[5],
+              Col3 = art_colours[2], Row1 = art_colours[6],
+              Row2 = art_colours[3], Row3 = art_colours[7],
+              Row4 = art_colours[4], Row5 = art_colours[8])
+chordDiagram(vis_data,
+             annotationTrack = NULL)
+
+circos.clear()
 }
+make_art(1)
